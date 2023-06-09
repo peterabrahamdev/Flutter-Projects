758bd5ea06acf361aed1ee541ad6ab0b2709da9c
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-06-09 19:47:51 +0200

    Update questions_screen.dart

--- Udemy/adv_basics/lib/widgets/questions_screen.dart
+++ Udemy/adv_basics/lib/widgets/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/widgets/answer_button.dart';
 import 'package:flutter/material.dart';
 
@@ -9,22 +10,33 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton('answer', (){})
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map(
+              (item) {
+                return AnswerButton(item, () { });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
