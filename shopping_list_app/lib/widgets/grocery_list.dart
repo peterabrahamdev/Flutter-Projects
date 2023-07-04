62f5769d4fd05ba6ee28b9954333a584f9600c9b
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-07-04 11:10:17 +0200

    Create grocery_list.dart

---
+++ shopping_list_app/lib/widgets/grocery_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/dummy_items.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: groceryItems.length,
+      itemBuilder: (ctx, index) {
+        return ListTile(
+          leading: Container(
+            height: 24,
+            width: 24,
+            color: groceryItems[index].category.color,
+          ),
+          title: Text(groceryItems[index].name),
+          trailing: Text(groceryItems[index].quantity.toString()),
+        );
+      },
+    );
+  }
+}
