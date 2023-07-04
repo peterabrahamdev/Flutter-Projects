49b41071e9aa70605e20955ef88a67bcd0527d11
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-07-04 23:01:13 +0200

    Update grocery_list.dart

--- shopping_list_app/lib/widgets/grocery_list.dart
+++ shopping_list_app/lib/widgets/grocery_list.dart
@@ -1,22 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
 
-class GroceryList extends StatelessWidget {
-  const GroceryList({super.key});
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key, required this.groceryItems});
+
+  final List<GroceryItem> groceryItems;
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _removeItem(GroceryItem groceryItem) {
+    final itemIndex = widget.groceryItems.indexOf(groceryItem);
+    setState(() {
+      widget.groceryItems.remove(groceryItem);
+    });
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Item deleted.'),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          setState(() {
+            widget.groceryItems.insert(itemIndex, groceryItem);
+          });
+        },
+      ),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: groceryItems.length,
+      itemCount: widget.groceryItems.length,
       itemBuilder: (ctx, index) {
-        return ListTile(
-          leading: Container(
-            height: 24,
-            width: 24,
-            color: groceryItems[index].category.color,
+        return Dismissible(
+          key: ValueKey(widget.groceryItems[index]),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            _removeItem(widget.groceryItems[index]);
+          },
+          background: Container(
+            color: Theme.of(context).colorScheme.error,
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
           ),
-          title: Text(groceryItems[index].name),
-          trailing: Text(groceryItems[index].quantity.toString()),
+          child: ListTile(
+            leading: Container(
+              height: 24,
+              width: 24,
+              color: widget.groceryItems[index].category.color,
+            ),
+            title: Text(widget.groceryItems[index].name),
+            trailing: Text(widget.groceryItems[index].quantity.toString()),
+          ),
         );
       },
     );
