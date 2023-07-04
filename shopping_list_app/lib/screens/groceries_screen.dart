c0201726a67c1b146170b48582b09fcaf3c88fa9
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-07-04 11:38:27 +0200

    Add appBar actions on the groceries screen

--- shopping_list_app/lib/screens/groceries_screen.dart
+++ shopping_list_app/lib/screens/groceries_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/screens/new_item_screen.dart';
 import 'package:shopping_list_app/widgets/grocery_list.dart';
 
-class GroceriesScreen extends StatelessWidget {
+class GroceriesScreen extends StatefulWidget {
   const GroceriesScreen({super.key});
+
+  @override
+  State<GroceriesScreen> createState() => _GroceriesScreenState();
+}
+
+class _GroceriesScreenState extends State<GroceriesScreen> {
+  void _addItem() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => const NewItemScreen(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Your groceries')),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
+        title: const Text('Your groceries'),
+      ),
       body: const GroceryList(),
     );
   }
