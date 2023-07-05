2deea98604bb583d6ba909f9f4e11df90e65f9fd
Peter Abraham <79099771+APeterIstvan@users.noreply.github.com>
2023-07-05 15:35:32 +0200

    Add backend data extraction function

--- shopping_list_app/lib/screens/groceries_screen.dart
+++ shopping_list_app/lib/screens/groceries_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/screens/new_item_screen.dart';
 import 'package:shopping_list_app/widgets/grocery_list.dart';
@@ -11,7 +15,57 @@ class GroceriesScreen extends StatefulWidget {
 }
 
 class _GroceriesScreenState extends State<GroceriesScreen> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _removeItem(GroceryItem groceryItem) {
+    final itemIndex = _groceryItems.indexOf(groceryItem);
+    setState(() {
+      _groceryItems.remove(groceryItem);
+    });
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: const Text('Item deleted.'),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          setState(() {
+            _groceryItems.insert(itemIndex, groceryItem);
+          });
+        },
+      ),
+    ));
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-9cd36-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category));
+    }
+    setState(() {
+      _groceryItems = loadedItems;
+      _isLoading = false;
+    });
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
@@ -31,19 +85,28 @@ class _GroceriesScreenState extends State<GroceriesScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (_isLoading) {
+      content = const Center(child: CircularProgressIndicator());
+    }
+
+    if (_groceryItems.isNotEmpty) {
+      content = GroceryList(
+        groceryItems: _groceryItems,
+        removeItem: _removeItem,
+      );
+    }
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: _addItem,
-            icon: const Icon(Icons.add),
-          )
-        ],
-        title: const Text('Your groceries'),
-      ),
-      body: _groceryItems.isEmpty
-          ? const Center(child: Text('Your list is empty. Add some items!'))
-          : GroceryList(groceryItems: _groceryItems),
-    );
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: _addItem,
+              icon: const Icon(Icons.add),
+            )
+          ],
+          title: const Text('Your groceries'),
+        ),
+        body: content);
   }
 }
